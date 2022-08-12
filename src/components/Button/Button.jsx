260dbac63e41373a3e7305633a04969bfa0f703a
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2022-08-12 18:48:17 +0100

    initial commit

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Button.css";
+
+const Button = ({ children, type = "button", onClick, buttonStyle }) => (
+	<button type={type} onClick={onClick} className={`btn-${buttonStyle}`}>
+		{children}
+	</button>
+);
+
+export default Button;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import Button from "../Button/Button";
+import "./NavBar.css";
+
+const NavBar = () => {
+	const [toggle, setToggle] = useState(false);
+
+	const navLinks = [
+		{ title: "Home", cName: "nav-link", url: "#" },
+		{ title: "Services", cName: "nav-link", url: "#" },
+		{ title: "Products", cName: "nav-link", url: "#" },
+		{ title: "Contact Us", cName: "nav-link", url: "#" },
+		{ title: "Sign Up", cName: "nav-link-mobile", url: "#" },
+	];
+
+	const onToggle = () => setToggle(!toggle);
+
+	return (
+		<nav className="navbar">
+			<h1 className="brand-logo">Navbar Redness</h1>
+			<div onClick={onToggle} className="menu-icon">
+				{toggle ? <div>&#x78;</div> : <div>&#x2630;</div>}
+			</div>
+			<ul className={`nav-menu ${toggle ? "active" : ""}`}>
+				{navLinks.map((item) => (
+					<li key={item.title}>
+						<a className={item.cName} href={item.url}>
+							{item.title}
+						</a>
+					</li>
+				))}
+			</ul>
+			<Button buttonStyle="primary">Sign Up</Button>
+		</nav>
+	);
+};
+
+export default NavBar;
