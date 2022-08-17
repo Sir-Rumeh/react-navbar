aa0bd1b94990f8938bcb2a67b5c17405a976a63f
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2022-08-17 08:11:08 +0100

    change nav components

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
 	return (
 		<div className="App">
 			<NavBar />
-			{console.log("Change component for NavBar")}
+			{/* {console.log("Change component for NavBar")} */}
 		</div>
 	);
 }
