66e8549d1a4e45ca64cf95935a8620b212c4a87e
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2022-09-24 09:58:27 +0100

    fix ui across browsers

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import "./Button.css";
 
-const Button = ({ children, type = "button", onClick, buttonStyle }) => (
-	<button type={type} onClick={onClick} className={`btn-${buttonStyle}`}>
-		{children}
-	</button>
-);
+// const Button = ({ children, type = "button", onClick, buttonStyle }) => (
+// 	<button type={type} onClick={onClick} className={`btn-${buttonStyle}`}>
+// 		{children}
+// 	</button>
+// );
 
 export default Button;
