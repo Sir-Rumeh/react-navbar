372ce03a21d209421bb33b65d0512f562ec2c7b2
Sir-Rumeh <nosarumenimuentinyan@gmail.com>
2023-01-05 10:29:43 +0100

    implemented additional features for web app

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -18,9 +18,9 @@ const NavBar = () => {
 	return (
 		<nav className="navbar">
 			{/* <h1 className="brand-logo">Navbar Redness</h1> */}
-			{/* <div onClick={onToggle} className="menu-icon">
+			<div onClick={onToggle} className="menu-icon">
 				{toggle ? <div>&#x78;</div> : <div>&#x2630;</div>}
-			</div> */}
+			</div>
 			<ul className={`nav-menu ${toggle ? "active" : ""}`}>
 				{navLinks.map((item) => (
 					<li key={item.title}>
